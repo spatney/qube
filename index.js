6bd774e28de0feb88a032c51af8987394cce7abb
Sachin Patney <sachinp@qualtrics.com>
2017-12-12 12:09:25 -0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+class Cube {
+    constructor(fromCube) { 
+        if(fromCube){
+
+        }
+    }
+
+    addRows(rows) {}
+
+    slice(){}
+
+    dice(){}
+
+    one(){}
+
+    exportCube(){}
+}
