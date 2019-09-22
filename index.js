1adf70bf18f819f54cb4f8fcb9f60eb724059303
Sachin Patney <spatney@microsoft.com>
2019-09-21 20:07:32 -0700

    rename + cache some vars locally

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Qube {
     constructor(options) {
         this.options = options;
         this.storage = 'in-memory'; // more to come
-        this.dimIndex = [
+        this.dimensionIndices = [
             options.dimensions[0].key,
             options.dimensions[1].key,
             options.dimensions[2].key
@@ -23,16 +23,17 @@ class Qube {
     }
 
     aggregateRow(row) {
-        const dim1 = row[this.dimIndex[0]];
-        const dim2 = row[this.dimIndex[1]];
-        const dim3 = row[this.dimIndex[2]];
-
         const cube = this.cube;
-        cube[dim1] = cube[dim1] || {};
-        cube[dim1][dim2] = cube[dim1][dim2] || {};
-        cube[dim1][dim2][dim3] = cube[dim1][dim2][dim3] || {};
-
         const measures = this.options.measures;
+        const dimensionIndices = this.dimensionIndices;
+        const dimensionOne = row[dimensionIndices[0]];
+        const dimensionTwo = row[dimensionIndices[1]];
+        const dimensionThree = row[dimensionIndices[2]];
+
+        cube[dimensionOne] = cube[dimensionOne] || {};
+        cube[dimensionOne][dimensionTwo] = cube[dimensionOne][dimensionTwo] || {};
+        cube[dimensionOne][dimensionTwo][dimensionThree] = cube[dimensionOne][dimensionTwo][dimensionThree] || {};
+
         for (const m of measures) {
             const key = m.key;
             const type = m.type;
@@ -40,8 +41,8 @@ class Qube {
 
             if (type === 'sum') {
                 const val = row[key];
-                cube[dim1][dim2][dim3][name] = cube[dim1][dim2][dim3][name] != null
-                    ? cube[dim1][dim2][dim3][name] + val
+                cube[dimensionOne][dimensionTwo][dimensionThree][name] = cube[dimensionOne][dimensionTwo][dimensionThree][name] != null
+                    ? cube[dimensionOne][dimensionTwo][dimensionThree][name] + val
                     : val;
             }
         }
@@ -50,47 +51,51 @@ class Qube {
     }
 
     slice(opts) {
-        const dims = opts.dimensions;
+        const dimensionIndices = this.dimensionIndices;
+        const cube = this.cube;
+        const options = this.options;
+
+        const dimensions = opts.dimensions;
         const measureName = opts.measure;
-        const dimKeys = Object.keys(dims);
-        const dim1Index = dimKeys.length === 2 ? this.dimIndex.findIndex(d => d === dimKeys[0]) : -1;
-        const dim2Index = dimKeys.length === 2 ? this.dimIndex.findIndex(d => d === dimKeys[1]) : -1;
+        const dimensionKeys = Object.keys(dimensions);
+        const firstDimensionIndex = dimensionKeys.length === 2 ? dimensionIndices.findIndex(d => d === dimensionKeys[0]) : -1;
+        const secondDimensionIndex = dimensionKeys.length === 2 ? dimensionIndices.findIndex(d => d === dimensionKeys[1]) : -1;
 
         let result;
 
-        let iKeys = Object.keys(this.cube) || [];
+        let iKeys = Object.keys(cube) || [];
 
-        if (dim1Index === 0)
-            iKeys = [dims[this.dimIndex[dim1Index]]].filter(x => iKeys.includes(x));
-        else if (dim2Index === 0)
-            iKeys = [dims[this.dimIndex[dim2Index]]].filter(x => iKeys.includes(x));
+        if (firstDimensionIndex === 0)
+            iKeys = [dimensions[dimensionIndices[firstDimensionIndex]]].filter(x => iKeys.includes(x));
+        else if (secondDimensionIndex === 0)
+            iKeys = [dimensions[dimensionIndices[secondDimensionIndex]]].filter(x => iKeys.includes(x));
 
         for (let i = 0; i < iKeys.length; i++) {
-            let fKeys = Object.keys(this.cube[iKeys[i]] || {}) || [];
-            if (dim1Index === 1)
-                fKeys = [dims[this.dimIndex[dim1Index]]].filter(x => fKeys.includes(x));
-            else if (dim2Index === 1)
-                fKeys = [dims[this.dimIndex[dim2Index]]].filter(x => fKeys.includes(x));
+            let fKeys = Object.keys(cube[iKeys[i]] || {}) || [];
+            if (firstDimensionIndex === 1)
+                fKeys = [dimensions[dimensionIndices[firstDimensionIndex]]].filter(x => fKeys.includes(x));
+            else if (secondDimensionIndex === 1)
+                fKeys = [dimensions[dimensionIndices[secondDimensionIndex]]].filter(x => fKeys.includes(x));
 
             for (let f = 0; f < fKeys.length; f++) {
-                let kKeys = Object.keys(this.cube[iKeys[i]][fKeys[f]] || {}) || [];
-                if (dim1Index === 2)
-                    kKeys = [dims[this.dimIndex[dim1Index]]].filter(x => kKeys.includes(x));
-                else if (dim2Index === 2)
-                    kKeys = [dims[this.dimIndex[dim2Index]]].filter(x => kKeys.includes(x));
+                let kKeys = Object.keys(cube[iKeys[i]][fKeys[f]] || {}) || [];
+                if (firstDimensionIndex === 2)
+                    kKeys = [dimensions[dimensionIndices[firstDimensionIndex]]].filter(x => kKeys.includes(x));
+                else if (secondDimensionIndex === 2)
+                    kKeys = [dimensions[dimensionIndices[secondDimensionIndex]]].filter(x => kKeys.includes(x));
 
                 for (let k = 0; k < kKeys.length; k++) {
 
-                    const diceValue = this.cube[iKeys[i]][fKeys[f]][kKeys[k]];
+                    const diceValue = cube[iKeys[i]][fKeys[f]][kKeys[k]];
 
                     if (diceValue != null) {
-                        const measureValue = this.cube[iKeys[i]][fKeys[f]][kKeys[k]][measureName];
+                        const measureValue = cube[iKeys[i]][fKeys[f]][kKeys[k]][measureName];
                         if (measureValue != null) {
                             if (result === undefined) {
                                 result = 0;
                             }
 
-                            if (this.options.measures.filter(d => d.name === measureName)[0].type === 'sum') {
+                            if (options.measures.filter(d => d.name === measureName)[0].type === 'sum') {
                                 result += measureValue;
                             }
                         }
@@ -103,16 +108,17 @@ class Qube {
     }
 
     dice(opts) {
-        const dims = opts.dimensions;
+        const dimensionIndices = this.dimensionIndices;
+        const dimensions = opts.dimensions;
         const measureName = opts.measure;
 
-        const dim1 = dims[this.dimIndex[0]];
-        const dim2 = dims[this.dimIndex[1]];
-        const dim3 = dims[this.dimIndex[2]];
+        const dimensionOne = dimensions[dimensionIndices[0]];
+        const dimensionTwo = dimensions[dimensionIndices[1]];
+        const dimensionThree = dimensions[dimensionIndices[2]];
 
         const cube = this.cube;
-        if (cube[dim1] && cube[dim1][dim2] && cube[dim1][dim2][dim3] && (cube[dim1][dim2][dim3][measureName] != null)) {
-            return cube[dim1][dim2][dim3][measureName];
+        if (cube[dimensionOne] && cube[dimensionOne][dimensionTwo] && cube[dimensionOne][dimensionTwo][dimensionThree] && (cube[dimensionOne][dimensionTwo][dimensionThree][measureName] != null)) {
+            return cube[dimensionOne][dimensionTwo][dimensionThree][measureName];
         }
 
         return undefined;
@@ -126,7 +132,7 @@ class Qube {
     serializeCube() {
         return JSON.stringify({
             options: this.options,
-            dimIndex: this.dimIndex,
+            dimIndex: this.dimensionIndices,
             storage: this.storage,
             cube: this.cube
         }, null, 2);
